2078d976f70877c78fdd62d0fb6256db493e2283
boshrahi <36932407+boshrahi@users.noreply.github.com>
2024-12-15 15:27:58 +0100

    adding pull refresh to home screen (#9)
    * adding pull refresh to home screen
    
    * Adding paging to video thumbnails grid list for movies (#8)
    
    * adding pull refresh to home screen

--- feature/home/src/main/java/io/boshra/home/HomeScreen.kt
+++ feature/home/src/main/java/io/boshra/home/HomeScreen.kt
@@ -1,12 +1,16 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package io.boshra.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,8 +18,12 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowForward
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,45 +31,60 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import io.boshra.filmtime.core.VideoThumbnailCard
-import io.boshra.filmtime.data.model.VideoListType
 import io.boshra.filmtime.data.model.VideoThumbnail
-import io.boshra.filmtime.data.model.VideoType
 
 @Composable
 fun HomeScreen(
   viewModel: HomeViewModel,
   onVideoThumbnailClick: (tmdbMovieId: Int) -> Unit,
-  onAllVideosClicked: (videoType: VideoType, videoList: VideoListType) -> Unit,
 ) {
   val state by viewModel.state.collectAsStateWithLifecycle()
 
-  if (state.isLoading) {
-    CircularProgressIndicator(
-      modifier = Modifier
-        .wrapContentSize(),
-    )
-  } else {
-    LazyColumn(
-      contentPadding = PaddingValues(top = 16.dp),
-      verticalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-      items(state.videoSections) { videoSection ->
-        VideoSectionRow(
-          title = videoSection.title,
-          items = videoSection.items,
-          onVideoThumbnailClick = {
-            onVideoThumbnailClick(it)
-          },
-          onAllVideosClicked = { videoType, videoList ->
-            onAllVideosClicked(videoType, videoList)
-          },
-        )
+  // PullRefreshState for managing refresh state
+  val pullRefreshState = rememberPullRefreshState(
+    refreshing = state.isRefreshing,
+    onRefresh = { viewModel.refreshData() }
+  )
+
+  Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+    if (state.isLoading) {
+      CircularProgressIndicator(
+        modifier = Modifier.wrapContentSize().align(Alignment.Center)
+      )
+    } else {
+      LazyColumn(
+        contentPadding = PaddingValues(top = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+      ) {
+        items(state.videoSections) { videoSection ->
+          VideoSectionRow(
+            title = videoSection.title,
+            items = videoSection.items,
+            onVideoThumbnailClick = {
+              onVideoThumbnailClick(it)
+            },
+          )
+        }
       }
     }
+  }
+  // PullRefreshIndicator for the refresh UI
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .pullRefresh(pullRefreshState)
+  ) {
+    // Your content (e.g., LazyColumn, CircularProgressIndicator, etc.)
+
+    PullRefreshIndicator(
+      refreshing = state.isRefreshing,
+      state = pullRefreshState,
+      modifier = Modifier.align(Alignment.TopCenter) // Correct alignment
+    )
   }
 }
 
@@ -71,14 +94,11 @@ fun VideoSectionRow(
   title: String,
   items: List<VideoThumbnail>,
   onVideoThumbnailClick: (tmdbMovieId: Int) -> Unit,
-  onAllVideosClicked: (videoType: VideoType, videoList: VideoListType) -> Unit,
 ) {
   Column {
     Row(
       modifier = Modifier
-        .clickable {
-          onAllVideosClicked(VideoType.Movie, VideoListType.Trending)
-        }
+        .clickable { }
         .padding(16.dp),
     ) {
       Text(
